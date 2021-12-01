ca6ac244f42b9c6cebae7038b5e489a4f22b86d0
halexandrucristian <heghesalexandrucristian@gmail.com>
2021-12-01 15:47:12 +0200

    .

--- Day1.py
+++ Day1.py
@@ -9,7 +9,7 @@ def dayOnePartOne():
         lastNumber = x
         totalNumber+=1
 
-    return increasedIndex-1
+    return increasedIndex - 1
 
 def dayOnePartTwo():
     lastSum = 0
@@ -19,14 +19,14 @@ def dayOnePartTwo():
     lines = f.readlines()
 
     i = 0
-    for i in range(len(lines)-2):
-        currentSum = int(lines[i])+int(lines[i+1])+int(lines[i+2])
+    for i in range(len(lines) - 2):
+        currentSum = int(lines[i]) + int(lines[i + 1]) + int(lines[i + 2])
         if (int(currentSum) > int(lastSum)):
             increasedIndex+=1
         lastSum = currentSum
-        i = i+3
+        i = i + 3
 
-    return increasedIndex-1
+    return increasedIndex - 1
 
 def main():
     print(dayOnePartOne())
