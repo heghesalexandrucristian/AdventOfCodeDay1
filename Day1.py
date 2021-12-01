c510c861398ec880f0b3b2b843bd69e40b677ffc
halexandrucristian <heghesalexandrucristian@gmail.com>
2021-12-01 15:44:56 +0200

    clean

--- Day1.py
+++ Day1.py
@@ -1,31 +1,36 @@
-"""
-lastNumber = 0
-increasedIndex = 0
-totalNumber = 0
-f = open("date.txt", "r")
-for x in f:
-    if (int(x) > int(lastNumber)):
-        increasedIndex+=1
-    lastNumber = x
-    totalNumber+=1
-print(totalNumber)
-print(increasedIndex-1)
-"""
-######### Part II ################
-lastSum = 0
-currentSum = 0
-increasedIndex = 0
-totalNumber = 0
-f = open("date.txt", "r")
-lines = f.readlines()
+def dayOnePartOne():
+    lastNumber = 0
+    increasedIndex = 0
+    totalNumber = 0
+    f = open("date.txt", "r")
+    for x in f:
+        if (int(x) > int(lastNumber)):
+            increasedIndex+=1
+        lastNumber = x
+        totalNumber+=1
 
-i = 0
-for i in range(len(lines)-2):
-    currentSum = int(lines[i])+int(lines[i+1])+int(lines[i+2])
-    if (int(currentSum) > int(lastSum)):
-        increasedIndex+=1
-    lastSum = currentSum
-    i = i+3
+    return increasedIndex-1
 
-print(totalNumber)
-print(increasedIndex-1)
+def dayOnePartTwo():
+    lastSum = 0
+    currentSum = 0
+    increasedIndex = 0
+    f = open("date.txt", "r")
+    lines = f.readlines()
+
+    i = 0
+    for i in range(len(lines)-2):
+        currentSum = int(lines[i])+int(lines[i+1])+int(lines[i+2])
+        if (int(currentSum) > int(lastSum)):
+            increasedIndex+=1
+        lastSum = currentSum
+        i = i+3
+
+    return increasedIndex-1
+
+def main():
+    print(dayOnePartOne())
+    print(dayOnePartTwo())
+
+if __name__ == "__main__":
+    main()
